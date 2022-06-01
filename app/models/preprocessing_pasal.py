467a6eef2d88cb7e72b8bc70fcb8f27ac6ade60e
Ajulll22 <ajulrizki@gmail.com>
2022-06-01 23:36:32 +0700

    model database preprocessing pasal

---
+++ app/models/preprocessing_pasal.py
@@ -0,0 +1,18 @@
+from sqlalchemy import (
+    Column,
+    ForeignKey,
+    Text,
+    Integer,
+    String,
+    Table
+)
+from app.config.db import metadata
+
+
+Preprocessing = Table(
+    'preprocessing_pasal',
+    metadata,
+    Column('id_prep_pasal', Integer, primary_key=True),
+    Column('id_uu_pasal', Integer, ForeignKey('uu_pasal_html.id')),
+    Column('uud_detail', Text)
+)
