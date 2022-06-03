a740654ce29d917f6831e31238df7ab03a194bd6
Ajulll22 <ajulrizki@gmail.com>
2022-06-04 01:29:49 +0700

    update routes

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -4,6 +4,9 @@ from app.config.db import database
 from app.api.harmonisasi.harmonisasi_file import harmonisasi_file, GetUUResponse
 from app.api.harmonisasi.harmonisasi_pasal import harmonisasi_pasal, HarmonisasiPasalResponse
 
+from app.api.preprocessing.tambah_bulk import tambah_bulk
+from app.api.preprocessing.tambah_uu import tambah_uu, TambahUUResponse
+
 api_router = APIRouter()
 
 
@@ -17,6 +20,11 @@ async def shutdown():
     await database.disconnect()
 
 api_router.add_api_route('/v1/harmonisasi/{file}', harmonisasi_file,
-                         methods=['GET'], tags=['Harmonisasi'], response_model=GetUUResponse)
+                         methods=['GET'], tags=['Harmonisasi'], response_model=GetUUResponse, description='Harmonisasi RUU ekstensi file pdf dengan UU yang telah ada di dalam database')
 api_router.add_api_route('/v1/harmonisasi/pasal', harmonisasi_pasal,
-                         methods=['POST'], tags=['Harmonisasi Pasal'], response_model=HarmonisasiPasalResponse)
+                         methods=['POST'], tags=['Harmonisasi'], response_model=HarmonisasiPasalResponse, description='Drafting dengan teknik harmonisasi menggunakan search key')
+
+api_router.add_api_route('/v1/preprocessing_bulk', tambah_bulk,
+                         methods=['POST'], tags=['Preprocessing'], status_code=201, description='Preprocessing pasal dengan request dict')
+api_router.add_api_route('/v1/preprocessing_uu', tambah_uu,
+                         methods=['POST'], tags=['Preprocessing'], response_model=TambahUUResponse, status_code=201, description='Preprocessing UU dan mengembalikan hasil ekstraksi text full')
